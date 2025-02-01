8edfde7ac4c9e250e36bece018a60ca0ba9cdaaf
Giovanni annaloro <99549477+giovannaloro@users.noreply.github.com>
2025-02-01 17:45:02 +0100

    fft extended to arbitraly lenght input array

--- fft.c
+++ fft.c
@@ -1,6 +1,12 @@
 #include <math.h>
 #include <complex.h>
 
+int next_power_oftwo(int len){
+    float e = log2(e);
+    int n = e;  
+    return e++;
+}
+
 //A simple implementation of fft for power of two size integers values
 complex double * fft(int * samples, int len){
     //Return the array if the size of the array is one 
@@ -9,6 +15,19 @@ complex double * fft(int * samples, int len){
         complex double * spectrum = malloc(sizeof(double complex));
         spectrum[0] = value;
         return spectrum;
+    }
+    //if the input array size is not a power of two fill 0s until the size reaches the next power of two dimension 
+    if(len%2 != 0){
+      int filled_samples_len = 2**next_power_oftwo(len);
+      int filled_samples [filled_samples_len];
+      for(int i = 0; i<len ; i++){
+          filled_samples[i] = samples[i];
+      }
+      for(int i = len ; i < filled_samples_len; i++){
+          filled_samples[i] = 0;
+      }
+      samples = filled_samples;
+      len = filled_samples_len; 
     }
     //Create an even array and an odd array of values and fill them with the odd/even elements of the array
     int even_samples[len/2];
